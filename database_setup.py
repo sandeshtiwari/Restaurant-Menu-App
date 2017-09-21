d001f3bbe2d6f133f7446e4c6a350783fb5a3d4c
Sandesh Tiwari <sandeshtiwari@live.com>
2017-09-21 16:10:33 -0500

    creating classes and tables

--- database_setup.py
+++ database_setup.py
@@ -6,7 +6,11 @@ from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
 Base = declerative_base()
 
+class Restaurant(Base):
+    __tablename__ = 'restaurant'
 
+class MenuItem(Base):
+    __tablename__ = 'menu_item'
 
 
 engine= create_engine('sqlite:///restaurantmenu.db')
