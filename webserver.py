4779e39d0d7a520382bea2c5aa68307ec1c314a3
Sandesh Tiwari <sandeshtiwari@live.com>
2017-09-25 10:24:19 -0500

    changed webserver to add new restaurants and added new restaurants

--- webserver.py
+++ webserver.py
@@ -65,6 +65,7 @@ class webserverHandler(BaseHTTPRequestHandler):
 				self.send_header('Content-type','text/html')
 				self.end_headers()
 				output = ""
+				output += "<a href = '/restaurants/new'>Make a new Restaurant Here</a></br></br>"
 				output += "<html><body>"
 				restaurants = session.query(Restaurant).all()
 				for restaurant in restaurants:
@@ -74,11 +75,38 @@ class webserverHandler(BaseHTTPRequestHandler):
 				output += "</body></html>"
 				self.wfile.write(output)
 				print(output)
+			if self.path.endswith("/restaurants/new"):
+				self.send_response(200)
+				self.send_header('Content-type', 'text/html')
+				self.end_headers()
+				output = ""
+				output += "<html><body>"
+				output += "<h1>Make a New Restaurant</h1>"
+				output += "<form method = 'POST' enctype = 'multipart/form-data' action = '/restaurants/new'>"
+				output += "<input name = 'newRestaurantName' type = 'text' placeholder = 'New Restaurant name'>"
+				output += "<input type = 'submit' value = 'Create'>"
+				output += "</body></html>"
+				self.wfile.write(output)
+				print(output)
 		except IOError:
 			#page not found error
 			self.send_error(404, "File not Found %s" % self.path)
 	def do_POST(self):
 		try:
+			if self.path.endswith("/restaurants/new"):
+				ctype, pdict = cgi.parse_header(self.headers.getheader('content-type'))
+				if ctype == 'multipart/form-data':
+					fields = cgi.parse_multipart(self.rfile, pdict)
+					messagecontent = fields.get("newRestaurantName")
+					#create a new restaurant
+					newRestaurant = Restaurant(name = messagecontent[0])
+					session.add(newRestaurant)
+					session.commit()
+					self.send_response(301)
+					self.send_header('Content-type', 'text/html')
+					self.send_header('Location', '/restaurants')
+					self.end_headers()
+			'''
 			#responding ny telling that there was a successfull POST request
 			self.send_response(301)
 			#end header response
@@ -102,7 +130,7 @@ class webserverHandler(BaseHTTPRequestHandler):
 			#sending output to the client
 			self.wfile.write(output)
 			print(output)
-
+			'''
 		except:
 			pass
 
