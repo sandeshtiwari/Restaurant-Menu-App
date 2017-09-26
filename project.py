f4c203e93eb938362533be4a32e9c9b898e17249
Sandesh Tiwari <sandeshtiwari@live.com>
2017-09-26 10:18:00 -0500

    added sqlalchemy to display all the items from the first restaurant

--- project.py
+++ project.py
@@ -1,10 +1,28 @@
 from flask import Flask
 app = Flask(__name__)
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+
 @app.route('/')
 @app.route('/hello')
 def HelloWorld():
-	return "Hello World"
+	restaurant = session.query(Restaurant).first()
+	items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+	output = ""
+	for item in items:
+		output += item.name + "</br>"
+		output += item.price + "</br>"
+		output += item.description + "</br>"
+		output += '</br></br>'
+	return output
 
 if __name__ == '__main__':
 	app.debug = True
