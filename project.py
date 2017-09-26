22b64db4dcf2eca77d028ab0f50ba4209b453afc
Sandesh Tiwari <sandeshtiwari@live.com>
2017-09-26 10:38:15 -0500

    added routes to delete, edit and add menu items

--- project.py
+++ project.py
@@ -10,9 +10,9 @@ Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
-
+#route to the root directory
 @app.route('/')
-@app.route('/restaurants/<int:restaurant_id>/')
+@app.route('/restaurant/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
 	restaurant = session.query(Restaurant).filter_by( id = restaurant_id).one()
 	items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
@@ -23,6 +23,18 @@ def restaurantMenu(restaurant_id):
 		output += item.description + "</br>"
 		output += '</br></br>'
 	return output
+
+@app.route('/restaurant/<int:restaurant_id>/new')
+def newMenuItem(restaurant_id):
+	return "Page to create a new menu item."
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
+	return "Page to edit a menu item"
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id, menu_id):
+	return "Page to delete a menu item"
 
 if __name__ == '__main__':
 	app.debug = True
