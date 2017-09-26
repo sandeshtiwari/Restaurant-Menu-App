e89e08b8179384f1cb0db307c56f6cf4d2a6d33a
Sandesh Tiwari <sandeshtiwari@live.com>
2017-09-26 10:27:56 -0500

    routing to different restaurants menu

--- project.py
+++ project.py
@@ -12,9 +12,9 @@ DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-	restaurant = session.query(Restaurant).first()
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+	restaurant = session.query(Restaurant).filter_by( id = restaurant_id).one()
 	items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
 	output = ""
 	for item in items:
