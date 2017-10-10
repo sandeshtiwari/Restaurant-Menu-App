de9c9175e01882a951769dfb27cc36f5cbbba621
Sandesh Tiwari <sandeshtiwari@live.com>
2017-10-10 10:27:58 -0500

    Added JSON for the restaurans

--- project.py
+++ project.py
@@ -18,6 +18,10 @@ def restaurantMenuJSON(restaurant_id):
         restaurant_id=restaurant_id).all()
     return jsonify(MenuItems=[i.serialize for i in items])
 
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON/')
+def menuItemJSON(restaurant_id, menu_id):
+	menuItem = session.query(MenuItem).filter_by(id = menu_id).one()
+	return jsonify(MenuItem = menuItem.serialize)
 
 #route to the root directory
 @app.route('/')
