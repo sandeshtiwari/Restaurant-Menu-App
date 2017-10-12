a020a3d19ae20908a72be3c763fb620fdf88a683
Sandesh Tiwari <sandeshtiwari@live.com>
2017-10-12 10:44:29 -0500

    Made all the rendering templates and imported sqlalchemy for crud functionality

--- finalProject.py
+++ finalProject.py
@@ -1,54 +1,50 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, newMenuItem
 
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
 
-#Fake Restaurants
-restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
-
-restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'},{'name':'Taco Hut', 'id':'3'}]
-
-
-#Fake Menu Items
-items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
-item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
-
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
 
 
 @app.route('/')
 @app.route('/restaurants')
 def showRestaurants():
-	restaurants = session.query(Restaurant)
-	return "This page will have list of restaurants"
+	return render_template('restaurants.html', restaurants = restaurants)
 
 @app.route('/restaurant/new')
 def newRestaurant():
-	return "This page will be used for making new restaurant"
+	return render_template('newRestaurant.html')
 
 @app.route('/restaurant/<int:restaurant_id>/edit')
 def editRestaurant(restaurant_id):
-	return "This page will be for editing restaurant %s" %restaurant_id
+	return render_template('editRestaurant.html', restaurant = restaurant)
 
 @app.route('/restaurant/<int:restaurant_id>/delete')
 def deleteRestaurant(restaurant_id):
-	return "This page will be for deleting restaurant %s" %restaurant_id
+	return render_template('deleteRestaurant.html', restaurant = restaurant)
 
 @app.route('/restaurant/<int:restaurant_id>')
 @app.route('/restaurant/<int:restaurant_id>/menu')
 def showMenu(restaurant_id):
-	return "This page is the menu for restaurant %s" %restaurant_id
+	return render_template('menu.html', restaurant= restaurant, items = items)
 
 @app.route('/restaurant/<int:restaurant_id>/menu/new')
 def newMenuItem(restaurant_id):
-	return "This page is for making a new menu item for restaurant %s" %restaurant_id
+	return render_template('newmenuitem.html')
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit')
 def editMenuItem(restaurant_id, menu_id):
-	return "This page is for editing menu of restaurnat with id {0} and menu with id {1}".format(restaurant_id,menu_id)
+	return render_template('editmenuitem.html', item = item)
 
 @app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete')
 def deleteMenuItem(restaurant_id, menu_id):
-	return "This page is for deleting menu of restaurnat with id {0} and menu with id {1}".format(restaurant_id,menu_id)
+	return render_template('deletemenuitem.html', item = item)
 
 
 if __name__ == '__main__':
