f2a8097508ff19ca26e7e297fd59e8a4efb3790b
Sandesh Tiwari <sandeshtiwari@live.com>
2017-10-13 10:46:02 -0500

    added serailzed method in the db setup file and added urls for json objects

--- database_setup.py
+++ database_setup.py
@@ -10,6 +10,12 @@ class Restaurant(Base):
     __tablename__ = 'restaurant'
     name = Column(String(80), nullable = False)
     id = Column(Integer, primary_key = True)
+    @property
+    def serialize(self):
+        return{
+            'name':self.name,
+            'id':self.id
+            }
     
 class MenuItem(Base):
     __tablename__ = 'menu_item'
--- finalProject.py
+++ finalProject.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for, flash, jsonify
 app = Flask(__name__)
 
 from sqlalchemy import create_engine
@@ -11,7 +11,21 @@ Base.metadata.bind = engine
 DBSession = sessionmaker(bind = engine)
 session = DBSession()
 
+@app.route('/restaurants/JSON')
+def resstaurantsJSON():
+	restaurants = session.query(Restaurant).all()
+	return jsonify(Restaurants=[restaurant.serialize for restaurant in restaurants])
 
+@app.route('/restaurants/<int:restaurant_id>/menu/JSON/')
+def restaurantMenuJSON(restaurant_id):
+	restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one()
+	items = session.query(MenuItem).filter_by(restaurant_id = restaurant_id).all()
+	return jsonify(MenuItems = [item.serialize for item in items])
+
+@app.route('/restaurants/<int:restaurant_id>/menu/<int:menu_id>/JSON/')
+def menuItemJSON(restaurant_id, menu_id):
+	menuItem = session.query(MenuItem).filter_by(id = menu_id).one()
+	return jsonify(MenuItem = menuItem.serialize)
 
 @app.route('/')
 @app.route('/restaurants', methods = ['GET', 'POST'])
