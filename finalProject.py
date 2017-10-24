252e2492b151d9fd4df73f894129c5e2fcd8abf7
Sandesh Tiwari <sandeshtiwari@live.com>
2017-10-24 15:25:44 -0500

    gmail connection and loggin in setup

--- finalProject.py
+++ finalProject.py
@@ -7,6 +7,16 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
 
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
+
+#reading the json file for client id
+CLIENT_ID = json.loads(open('client_secrets.json','r').read())['web']['client_id']
+
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
 
@@ -17,7 +27,69 @@ session = DBSession()
 def showLogin():
 	state = ''.join(random.choice(string.ascii_uppercase + string.digits) for x in xrange(32))
 	login_session['state'] = state
-	return "The current session state is %s" % login_session['state']
+	return render_template('login.html', STATE=state)
+
+@app.route('/gconnect/', methods = ['POST'])
+def gconnect():
+	print('in')
+	if request.args.get('state') != login_session['state']:
+		response = make_response(json.dumps('Invalid state parameter'), 401)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+	code = request.data
+	try:
+		oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+		oauth_flow.redirect_uri = 'postmessage'
+		credentials = oauth_flow.step2_exchange(code)
+	except FlowExchangeError:
+		response = make_response(json.dumps('Failed to upgrade the authorization code.'), 401)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+	#check that the access token is valid
+	access_token = credentials.access_token
+	url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s' % access_token)
+	h = httplib2.Http()
+	result = json.loads(h.request(url, 'GET')[1])
+	#if there was an erro in the access token info, abort
+	if result.get('error') is not None:
+		response = make_response(json.dumps(reslut.get('error')), 50)
+		response_headers['Content-Type'] = 'application/json'
+	#verify that the access token is used for the intended user
+	gplus_id = credentials.id_token['sub']
+	if result['user_id'] != gplus_id:
+		response = make_response(json.dumps("Token's user ID doesn't match given user ID"), 401)
+		response.headers['Content-Type'] = 'application/json'
+		return response
+	#check to see if user is already logged in
+	stored_credentials = login_session.get('credentials')
+	stored_gplus_id = login_session.get('gplus_id')
+	if stored_credentials is not None and gplus_id == stored_gplus_id:
+		response = make_response(json.dumps('Current user is already connected'), 200)
+		response.headers['Content-Type'] = 'application/json'
+	#store the accesss token in the session for later use.
+	login_session['access_token'] = credentials.access_token
+	login_session['gplus_id'] = gplus_id
+
+	#get user info from google
+	userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+	params = {'access_token': credentials.access_token, 'alt': 'json'}
+	answer = requests.get(userinfo_url, params=params)
+	data = answer.json()
+
+	login_session['username'] = data['name']
+	login_session['picture'] = data['picture']
+	login_session['email'] = data['email']
+	output = ''
+	output += '<h1>Welcome, '
+	output += login_session['username']
+	output += '!</h1>'
+	output += '<img src="'
+	output += login_session['picture']
+	output += ' " style = "width: 300px; height: 300px;border-radius: 150px;-webkit-border-radius: 150px;-moz-border-radius: 150px;"> '
+	flash("you are now logged in as %s" % login_session['username'])
+	print "done!"
+	return output
+
 
 @app.route('/restaurants/JSON')
 def resstaurantsJSON():
@@ -36,7 +108,7 @@ def menuItemJSON(restaurant_id, menu_id):
 	return jsonify(MenuItem = menuItem.serialize)
 
 @app.route('/')
-@app.route('/restaurants', methods = ['GET', 'POST'])
+@app.route('/restaurants/', methods = ['GET', 'POST'])
 def showRestaurants():
 	restaurants = session.query(Restaurant)
 	rows = session.query(Restaurant).count()
