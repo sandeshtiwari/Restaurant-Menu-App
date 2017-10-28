918df18455554c244b724b96e91686970420ba07
Sandesh Tiwari <sandeshtiwari@live.com>
2017-10-28 09:21:38 -0500

    wrote functions to get userInfo and userID

--- finalProject.py
+++ finalProject.py
@@ -238,6 +238,17 @@ def deleteMenuItem(restaurant_id, menu_id):
 	else:
 		return render_template('deletemenuitem.html', item = deleteItem, restaurant_id = restaurant_id)
 
+def getUserID(email):
+	try:
+		user = session.query(User).filter_by(email = email).one()
+		return user.id
+	except:
+		return None
+
+def getUserInfo(user_id):
+	user = session.query(User).filter_by(id = user_id).one()
+	return user
+
 def createUser(login_session):
 	newUser = User(name = login_session['username'], email = login_session['email'],picture =
 		login_session['picture'])
