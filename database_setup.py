85b5a1c34f85b7e7fb08cf32953c4a42464d8efb
Sandesh Tiwari <sandeshtiwari@live.com>
2017-10-28 09:08:08 -0500

    Added users system to the database_setup.py file

--- database_setup.py
+++ database_setup.py
@@ -1,36 +1,55 @@
-import os
-import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+
 Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
 
 class Restaurant(Base):
     __tablename__ = 'restaurant'
-    name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
     @property
     def serialize(self):
-        return{
-            'name':self.name,
-            'id':self.id
-            }
-    
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
 class MenuItem(Base):
     __tablename__ = 'menu_item'
-    name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
-    course = Column(String(250))
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String(250))
     price = Column(String(8))
+    course = Column(String(250))
     restaurant_id = Column(Integer, ForeignKey('restaurant.id'))
     restaurant = relationship(Restaurant)
-    #this is a serialize function to be able to send JSON objects in a serializable format
-    @property 
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
     def serialize(self):
-        return{
-            'name':self.name,
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
             'description': self.description,
             'id': self.id,
             'price': self.price,
@@ -38,5 +57,7 @@ class MenuItem(Base):
         }
 
 
-engine= create_engine('sqlite:///restaurantmenu.db')
+engine = create_engine('sqlite:///restaurantmenuwithusers.db')
+
+
 Base.metadata.create_all(engine)
